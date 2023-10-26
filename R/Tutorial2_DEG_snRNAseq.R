2d4e4a73bb5b7f34ad836720f16156bdde4a8723
Dario <dario.righelli@gmail.com>
2023-10-26 16:57:00 +0200

    adding description of dataset

---
+++ R/Tutorial2_DEG_snRNAseq.R
@@ -0,0 +1,11 @@
+#' Tutorial2_DEG_snRNAseq
+#' @description this dataset contains the psctrl data.frame including
+#' positive controls, neg_ntrl which is a dataframe of negative controls and
+#' pb which is a SingleCellExperiment of data in pseudo-bulk format
+#' @name Tutorial2_DEG_snRNAseq
+#' @aliases neg_ctrl, pb, posctrl
+#' @docType data
+#' @author My Name \email{blahblah@@roxygen.org}
+#' @references \url{data_blah.com}
+#' @keywords data
+NULL
